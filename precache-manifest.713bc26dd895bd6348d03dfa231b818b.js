06231b2a62c311c345fb7314a225fc12276846d2
informatica2 <informatica@unahur.edu.ar>
2020-09-12 15:40:48 -0300

    Updates

---
+++ precache-manifest.713bc26dd895bd6348d03dfa231b818b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6655d91bcc448f179e70e009a3ee0f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e1f64afa1376a7b6444f",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "277b59e54c40461a1069",
+    "url": "/my-app/static/js/2.85b74edc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.85b74edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f64afa1376a7b6444f",
+    "url": "/my-app/static/js/main.f25e0a4a.chunk.js"
+  },
+  {
+    "revision": "fcb089ad4f4056fc424d",
+    "url": "/my-app/static/js/runtime-main.01f5c011.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
